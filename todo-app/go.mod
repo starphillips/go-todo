7bb1a38dfc6759d0a39ad311da4ab69c79b90911
starkie.phillips <starkiephillips@callenlenz.com>
2026-02-24 12:53:53 +0000

    subcommands and show list func

--- todo-app/go.mod
+++ todo-app/go.mod
@@ -1,3 +1,5 @@
 module todo-app
 
 go 1.25.0
+
+require github.com/rodaine/table v1.3.0
--- todo-app/main.go
+++ todo-app/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"strconv"
 	"time"
+
+	"github.com/rodaine/table"
 )
 
 // the type for one task (blueprint)
@@ -19,6 +21,7 @@ type Todo struct {
 type Todos []Todo
 
 // td - the variable holding the list - refers to the actual instance of your todo list - the actual list you're modifying
+
 func (td *Todos) add(title string) {
 	item := Todo{ //  the variable holding one new task
 		ID:          len(*td) + 1,
@@ -33,12 +36,15 @@ func (td *Todos) add(title string) {
 
 // delete
 func (td *Todos) delete(id int) {
-	for i := 0; i < len(*td); {
-		if item.ID == id {
-
+	for i := 0; i < len(*td); i++ {
+		if (*td)[i].ID == id {
+			newList := append((*td)[:i], (*td)[i+1:]...) // Creates a new slcie without i (the deleted task)
+			*td = newList
+			fmt.Printf("Item deleted")
+			return
 		}
-
 	}
+	fmt.Printf("There is no matching ID to delete")
 	// check IDs
 	// if ID matches the one being requested to be deleted on the command line, then delete it
 	// if not check the next ID
@@ -46,12 +52,36 @@ func (td *Todos) delete(id int) {
 
 // complete
 func (td *Todos) complete(id int) {
+	for i := 0; i < len(*td); i++ {
+		if (*td)[i].ID == id {
+			(*td)[i].Completed = true
+			fmt.Printf("Item marked as complete")
+			return
+		}
+	}
+	fmt.Printf("There is no matching ID to complete")
 	// check ids
-	// if ID matches the one being requested to be completed on the command line, then delete it
+	// if ID matches the one being requested to be completed on the command line, then cahnge its complete to completed
 	// if not check the next ID
 }
 
 // func display all tasks
+func (td *Todos) showList() {
+	tbl := table.New()
+	tbl.AddRow("ID", "Title", "Completed?", "Created At", "Completed At")
+
+	for _, item := range *td {
+		tbl.AddRow(
+			item.ID,
+			item.Title,
+			item.Completed,
+			item.CreatedAt.Format(time.RFC822),
+			item.CompletedAt.Format(time.RFC822),
+		)
+
+	}
+	tbl.Print()
+}
 
 // subcommands to call these funcs
 
@@ -67,6 +97,7 @@ var commands = []command{
 	{Name: "add", Help: "add an item", Run: addCmd(&td)},
 	{Name: "delete", Help: "delete an item", Run: deleteCmd(&td)},
 	{Name: "done", Help: "mark an item as done", Run: completeCmd(&td)},
+	{Name: "list", Help: "shwos all items on your todo list", Run: completeCmd(&td)},
 }
 
 func addCmd(td *Todos) func(args []string) error {
@@ -117,3 +148,14 @@ func completeCmd(td *Todos) func(args []string) error {
 	// parse args to get the ID of the task to be completed
 	// call the complete func with the ID
 }
+
+func listCmd(td *Todos) func(args []string) error {
+	return func(args []string) error {
+		td.showList()
+		return nil
+	}
+}
+
+// implementing commands
+
+// https://medium.com/data-science/how-to-create-a-cli-in-golang-with-cobra-d729641c7177
